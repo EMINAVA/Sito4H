9845e4bd2fc96e1e2526787ed3aa7b6af4f75003
EMINAVA <emiliano230104@gmail.com>
2020-10-10 13:53:09 +0200

    Migliorata barra di ricerca

---
+++ js/clearScript.js
@@ -0,0 +1,17 @@
+const searchBar = document.querySelector("#new-searchbar")
+const clearImg = document.querySelector("#clear-icon")
+
+searchBar.addEventListener("keyup", function() {
+    if (!searchBar.value) {
+        clearImg.style.visibility = "hidden";
+    } else {
+        clearImg.style.visibility = "visible";
+    }
+})
+
+
+clearImg.addEventListener("click", function() {
+    searchBar.value = "";
+    clearImg.style.visibility = "hidden";
+    searchBar.focus();
+})
